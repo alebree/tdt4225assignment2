d4551e06eee77eff73a1b6265c91c99889bff7f3
Alex <alexrex11@gmail.com>
2021-10-07 14:20:03 +0200

    added code for task 9b)

--- parttwo_four_ten.py
+++ parttwo_four_ten.py
@@ -20,6 +20,44 @@ class parttwo:
         print("Solution is ", rows[0][0])
         return
     
+    def task9b_user62(self):
+        #get activities from user 62
+        select_activities_62 = "Select id FROM (Select id, user_id, yearmonth FROM (Select id, user_id, EXTRACT(YEAR_MONTH FROM start_date_time) as yearmonth FROM Activity) as A) as B where (yearmonth = '200811' AND user_id = '062');"
+        self.cursor.execute(select_activities_62)
+        rows = self.cursor.fetchall()
+        activity_ids = []
+        for i in rows:
+            activity_ids.append(i[0])
+        #print(activity_ids)
+        import datetime
+        totaltime = datetime.timedelta(0,0,0)
+        for id in activity_ids:
+            select_time_62 = "Select * FROM Activity Where id = %s;" %(id)
+            self.cursor.execute(select_time_62)
+            rows = self.cursor.fetchall()
+            timedelta = rows[0][4]-rows[0][3]
+            totaltime += timedelta
+        return print('Total hours recorded user 62: ', totaltime)
+
+    def task9b_user128(self):
+        #get activities from user 128
+        select_activities_62 = "Select id FROM (Select id, user_id, yearmonth FROM (Select id, user_id, EXTRACT(YEAR_MONTH FROM start_date_time) as yearmonth FROM Activity) as A) as B where (yearmonth = '200811' AND user_id = '128');"
+        self.cursor.execute(select_activities_62)
+        rows = self.cursor.fetchall()
+        activity_ids = []
+        for i in rows:
+            activity_ids.append(i[0])
+        #print(activity_ids)
+        import datetime
+        totaltime = datetime.timedelta(0,0,0)
+        for id in activity_ids:
+            select_time_128 = "Select * FROM Activity Where id = %s;" %(id)
+            self.cursor.execute(select_time_128)
+            rows = self.cursor.fetchall()
+            timedelta = rows[0][4]-rows[0][3]
+            totaltime += timedelta
+        return print('Total hours recorded user 128: ', totaltime)        
+
     def task10(self):
         select_query = "Select * FROM Activity WHERE (user_id = 112 AND transportation_mode = 'walk');" # All Activities in 2008
         self.cursor.execute(select_query)
@@ -52,7 +90,8 @@ def main():
     program = None
     try:
         program = parttwo()
-        program.task10()
+        program.task9b_user62()
+        program.task9b_user128()
         #program.match_activity_labels()
     except Exception as e:
         print("ERROR: Failed to use database:", e)
